42bf31dfda584c7f6eef6db9aa2cfe054ab1609f
sobek1985 <sobek1985@gmail.com>
2015-08-04 13:08:04 +0100

    Grunt for sass compile

--- MikeRobbins.SPEAKDemo/Controllers/NewsArticlesController.cs
+++ MikeRobbins.SPEAKDemo/Controllers/NewsArticlesController.cs
@@ -7,11 +7,6 @@ namespace MikeRobbins.SPEAKDemo.Controllers
     [ServicesController]
     public class NewsArticlesController : EntityService<NewsArticle>
     {
-     
-        //public NewsArticlesController()
-        //    : this(new NewsArticleRepository())
-        //{
-        //}
         public NewsArticlesController(IRepository<NewsArticle> repository) : base(repository)
         {
         }
---
+++ MikeRobbins.SPEAKDemo/gruntfile.js
@@ -0,0 +1,25 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'style/style.css': 'sass/style.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-sass");
+};
